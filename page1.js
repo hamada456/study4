3b043f701d7bb1d4d105dbd9912fce34711ecea5
hamada456 <73261938+hamada456@users.noreply.github.com>
2021-07-16 16:58:07 +0900

    Update page1.js

--- page1.js
+++ page1.js
@@ -1,29 +1,36 @@
+/**
+ * 画像を表示して製品を紹介
+ */
 
-const $logo = document.getElementsByClassName("logo")
-const $companyName = document.getElementsByClassName("companyName")
+//(() => {
 
-//会社ロゴをnoneに初期化
-for(let i = 0 ; i < $logo.length ; i++){
-    $logo[i].style.display = "none";
-}
+    const $logo = document.getElementsByClassName("logo")
+    const $companyName = document.getElementsByClassName("companyName")
 
-//会社ロゴをnoneならblockでblockならnone
-for(let ii = 0 ; ii < $logo.length ; ii++){
-    $companyName[ii].addEventListener("click", () => {
-        if($logo[ii].style.display === "none") {
-            $logo[ii].style.display = "block";
-        }else{
-            $logo[ii].style.display = "none"
-        }
-});
-}
+    //会社ロゴをnoneに初期化
+    for(let i = 0 ; i < $logo.length ; i++){
+        $logo[i].style.display = "none";
+    }
 
-//会社ロゴをクリックで別タブにyoutube動画を表示
-for(let iii = 0 ; iii < $logo.length ; iii++) {
-    $logo[iii].addEventListener("click", () => {
-        let okNg = window.confirm( document.getElementById(iii).textContent + "のゲームの紹介ページを開きます。");
-            if(okNg){
-                window.open("http://192.168.1.112/study/pageB.html?" + encodeURIComponent(iii));
+    //会社ロゴをnoneならblockでblockならnone
+    for(let ii = 0 ; ii < $logo.length ; ii++){
+        $companyName[ii].addEventListener("click", () => {
+            if($logo[ii].style.display === "none") {
+                $logo[ii].style.display = "block";
+            }else{
+                $logo[ii].style.display = "none"
             }
     });
-}
+    }
+
+    //会社ロゴをクリックで別タブにyoutube動画を表示
+    for(let iii = 0 ; iii < $logo.length ; iii++) {
+        $logo[iii].addEventListener("click", () => {
+            let okNg = window.confirm( document.getElementById(iii).textContent + "のゲームの紹介ページを開きます。");
+                if(okNg){
+                    window.open("http://192.168.1.112/study/pageB.html?" + encodeURIComponent(iii));
+                }
+        });
+    }
+
+//});
